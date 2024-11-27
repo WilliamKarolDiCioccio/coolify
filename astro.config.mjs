b77e7757b365bd5f43087404963a33a9d7c73bac
Wilielmus <88447902+WilliamKarolDiCioccio@users.noreply.github.com>
2024-11-27 19:06:01 +0100

    Add Vercel adapter for serverless deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,11 @@ import tailwind from '@astrojs/tailwind';
 
 import preact from '@astrojs/preact';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()]
+  integrations: [tailwind(), preact()],
+  output: 'server',
+  adapter: vercel()
 });
